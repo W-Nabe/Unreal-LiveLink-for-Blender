ed0c0cf282084d150034bd71a558af17c4cea087
nabeyan <nabe@lean-system.co.jp>
2025-02-06 02:52:09 +0900

    fixed (result of constraints in Blender)

--- BlenderLiveLinkAddon.py
+++ BlenderLiveLinkAddon.py
@@ -1,8 +1,8 @@
 bl_info = {
     "name": "LiveLink",
     "author": "Viga Entertainment Technology",
-    "version": (1, 0),
-    "blender": (2, 80, 0),
+    "version": (1, 1),
+    "blender": (4, 3, 0),
     "location": "View3D > LiveLink",
     "description": "Live link setup between Blender and Unreal",
     "warning": "",
@@ -12,6 +12,7 @@ bl_info = {
 
 import bpy
 import math
+from mathutils import Matrix
 from socket import *
 
 # Common functions
@@ -22,8 +23,8 @@ def MessageBox(message = "", title = "Message Box", icon = 'INFO'):
         self.layout.label(text=message)
 
     bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
-    
-#Static Mesh Panel
+
+# Static Mesh Panel
 sub=[]
 sub_names=[]
 names=[]
@@ -34,21 +35,21 @@ def Sub_update(self,context):
                 if obj.type== 'MESH':
                     sub.append((obj.name,obj.name,""))
                     sub_names.append(obj.name)
-                   
+
                 else:
                     return{'CANCELLED'}
-          
+
 def returnSub(self,context):
     res=[]
     global names
     for i in sub:
       if i not in res:
          res.append(i)
-         
+
     for j in sub_names:
       if j not in names:
          names.append(j)
-         
+
     return res
 
 def removeSub(self,context):
@@ -59,7 +60,7 @@ def removeSub(self,context):
        sub.remove(a)
        sub_names.remove(temp)
        names.remove(temp)
-        
+
 class AddSubjects(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "object.subjects_register"
@@ -72,7 +73,7 @@ class AddSubjects(bpy.types.Operator):
     def execute(self, context):
         Sub_update(self,context)
         return{'FINISHED'}
-    
+
 class RemoveSubjects(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "object.subjects_removal"
@@ -85,30 +86,30 @@ class RemoveSubjects(bpy.types.Operator):
     def execute(self, context):
         removeSub(self,context)
         return{'FINISHED'}
-    
-class StaticProperties(bpy.types.PropertyGroup): 
-    
+
+class StaticProperties(bpy.types.PropertyGroup):
+
     my_enum  : bpy.props.StringProperty(name="Mesh Type",default="O")
-        
+
     my_enum2 : bpy.props.EnumProperty(
         name = "Control",
         description = "enum desc",
         items = [("SM","Single Mesh","")]
     )
-    
+
     my_string : bpy.props.EnumProperty(
         name = "Subjects",
         description = "enum desc",
         items=returnSub,
         default=None
-    )   
+    )
 
-GetIconStatic="RADIOBUT_OFF"    
+GetIconStatic="RADIOBUT_OFF"
 class LiveLinkStatic(bpy.types.Panel):
     #bl_parent_id = "BlenderUE LiveLink"
     bl_label = "Static Mesh"
-    bl_space_type = "VIEW_3D"   
-    bl_region_type = "UI"    
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
     bl_category  = "LiveLink"
     bl_options = {"DEFAULT_CLOSED"}
 
@@ -131,7 +132,7 @@ class LiveLinkStatic(bpy.types.Panel):
         box.operator(AddSubjects.bl_idname, text="Add subjects",icon='ADD')
         #row=layout.row()
         box1=row.box()
-        box1.operator(RemoveSubjects.bl_idname, text="Remove subject",icon='REMOVE')      
+        box1.operator(RemoveSubjects.bl_idname, text="Remove subject",icon='REMOVE')
         row=layout.row()
         box2=row.box()
         box2.operator("wm.modal_timer_operator", text="Start Live Link",icon=GetIconStatic)
@@ -156,7 +157,7 @@ class StopLiveLink(bpy.types.Operator):
         global cancelstatic
         cancelstatic=True
         return{'FINISHED'}
-    
+
 class ModalTimerOperator(bpy.types.Operator):
     """Operator which runs its self from a timer"""
     bl_idname = "wm.modal_timer_operator"
@@ -165,9 +166,9 @@ class ModalTimerOperator(bpy.types.Operator):
     host = "127.0.0.1" # set to IP address of target computer
     port = 2000
     addr = (host, port)
-    UDPSock = socket(AF_INET, SOCK_DGRAM) 
-    _timer = None      
-    
+    UDPSock = socket(AF_INET, SOCK_DGRAM)
+    _timer = None
+
     def modal(self, context, event):
         mytool = context.scene.my_tool2
         global GetIconStatic
@@ -177,10 +178,10 @@ class ModalTimerOperator(bpy.types.Operator):
             self.cancel(context)
             cancelstatic=False
             return {'CANCELLED'}
-        
+
         if event.type == 'TIMER':
             #bpy.data.objects["Cube"]
-            if(mytool.my_string!=""):                    
+            if(mytool.my_string!=""):
                 r=bpy.data.objects[mytool.my_string].rotation_euler
                 rotx=math.degrees(r.x)
                 roty=math.degrees(r.y)
@@ -188,16 +189,16 @@ class ModalTimerOperator(bpy.types.Operator):
                 message = mytool.my_enum + "_"+mytool.my_string+"="
                 message+="(" + str(-(bpy.data.objects[mytool.my_string].location.x*100)) + "," + str(bpy.data.objects[mytool.my_string].location.y*100) +  "," + str(bpy.data.objects[mytool.my_string].location.z*100) +  "," + str(-rotx) +  "," + str(roty)+  "," + str(-rotz) + "," +str(bpy.data.objects[mytool.my_string].scale.x)+ "," +str(bpy.data.objects[mytool.my_string].scale.y)+ "," +str(bpy.data.objects[mytool.my_string].scale.z)+ ")" + "||"
                 GetIconStatic="RADIOBUT_ON"
-                
+
             elif(mytool.my_enum=="O" and mytool.my_string==""):
                 MessageBox("No static mesh selected","Subjects")
                 GetIconStatic="RADIOBUT_OFF"
                 self.cancel(context)
                 return {'CANCELLED'}
-            
-            print(message)          
+
+            print(message)
             self.UDPSock.sendto(message.encode(), self.addr)
-            
+
         return {'PASS_THROUGH'}
 
     def execute(self, context):
@@ -218,7 +219,7 @@ sub_Arm=[]
 sub_names_Arm=[]
 names_Arm=[]
 
-#Add armature names to array
+# Add armature names to array
 def Sub_update_Arm(self,context):
         list=bpy.context.selected_objects
         global sub_names_Arm
@@ -226,21 +227,21 @@ def Sub_update_Arm(self,context):
                 if obj.type=='ARMATURE':
                     sub_Arm.append((obj.name,obj.name,""))
                     sub_names_Arm.append(obj.name)
-                   
+
                 else:
                     return{'CANCELLED'}
-          
+
 def returnSubArm(self,context):
     res=[]
     global names_Arm
     for i in sub_Arm:
       if i not in res:
          res.append(i)
-         
+
     for j in sub_names_Arm:
       if j not in names_Arm:
          names_Arm.append(j)
-    
+
     return res
 
 def removeSubArm(self,context):
@@ -266,7 +267,7 @@ class AddSubjects_Skeletal(bpy.types.Operator):
         print(context.scene.my_tool.my_enum)
         Sub_update_Arm(self,context)
         return{'FINISHED'}
-    
+
 class RemoveSubjects_Skeletal(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "object.subjects_removal_skeletal"
@@ -280,36 +281,43 @@ class RemoveSubjects_Skeletal(bpy.types.Operator):
         removeSubArm(self,context)
         return{'FINISHED'}
 
-class SkeletalProperties(bpy.types.PropertyGroup): 
-    
+class SkeletalProperties(bpy.types.PropertyGroup):
+
     my_enum  : bpy.props.StringProperty(name="Mesh Type",default="A")
-    
+
     my_enum1 : bpy.props.EnumProperty(
         name = "Bone Type",
         description = "enum desc",
         items = [("||","Armature","")]
     )
-    
+
     my_enum2 : bpy.props.EnumProperty(
         name = "Control",
         description = "enum desc",
         items = [("BC","Bone Control (Individual Animation)",""),
         ("AN","Simultaneous Animation","")]
     )
-    
+
     my_string : bpy.props.EnumProperty(
         name = "Subjects",
         description = "enum desc",
         items=returnSubArm,
         default=None
-    ) 
+    )
+
+    # New property for bone names
+    bone_names : bpy.props.StringProperty(
+        name="Bones",
+        description="Comma-separated list of bone names to send",
+        default=""
+    )
 
 GetIcon="RADIOBUT_OFF"
 class LiveLinkSkeletal(bpy.types.Panel):
     #bl_parent_id = "BlenderUE LiveLink"
     bl_label = "Skeletal Mesh"
-    bl_space_type = "VIEW_3D"   
-    bl_region_type = "UI"    
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
     bl_category  = "LiveLink"
     bl_options = {"DEFAULT_CLOSED"}
 
@@ -324,16 +332,18 @@ class LiveLinkSkeletal(bpy.types.Panel):
         row.label(text="IP Address : 127.0.0.1")
         row=layout.row()
         row.label(text="Port : 2000")
-        
+
         layout.prop(mytool,"my_enum1")
         layout.prop(mytool,"my_enum2")
         layout.prop(mytool,"my_string")
+        # Display the new bone names input field
+        layout.prop(mytool, "bone_names")
         row=layout.row()
         box=row.box()
         box.operator(AddSubjects_Skeletal.bl_idname, text="Add subjects",icon='ADD')
         #row=layout.row()
         box1=row.box()
-        box1.operator(RemoveSubjects_Skeletal.bl_idname, text="Remove subject",icon='REMOVE')      
+        box1.operator(RemoveSubjects_Skeletal.bl_idname, text="Remove subject",icon='REMOVE')
         row=layout.row()
         box2=row.box()
         box2.operator("wm.modal_timer_operator_skeletal", text="Start Live Link",icon=GetIcon)
@@ -342,7 +352,7 @@ class LiveLinkSkeletal(bpy.types.Panel):
         box3.operator(StopLiveLinkSkeletal.bl_idname, text="Stop Live Link",icon='X')
         # row=layout.row()
         # row.prop(context.scene, prop_name)
-        
+
 message1=""
 class SkeletalMeshModalTimer(bpy.types.Operator):
     """Operator which runs its self from a timer"""
@@ -352,9 +362,39 @@ class SkeletalMeshModalTimer(bpy.types.Operator):
     host = "127.0.0.1" # set to IP address of target computer
     port = 2000
     addr = (host, port)
-    UDPSock = socket(AF_INET, SOCK_DGRAM) 
-    _timer = None      
-    
+    UDPSock = socket(AF_INET, SOCK_DGRAM)
+    _timer = None
+
+    def get_pose_matrix_in_pose_space(pose_bone):
+        # Get bone and armature references
+        bone = pose_bone.bone
+        arm = pose_bone.id_data
+
+        # Get rest matrices
+        bone_ml = bone.matrix_local
+        if bone.parent:
+            parent_ml = bone.parent.matrix_local
+        else:
+            parent_ml = Matrix.Identity(4)
+
+        # Get pose matrices
+        bone_mp = pose_bone.matrix
+        if bone.parent:
+            parent_mp = pose_bone.parent.matrix
+        else:
+            parent_mp = Matrix.Identity(4)
+
+        # Calculate local transforms relative to parent in rest pose
+        local_rest = parent_ml.inverted() @ bone_ml
+
+        # Calculate local transforms relative to parent in posed state
+        local_posed = parent_mp.inverted() @ bone_mp
+
+        # Calculate difference between posed and rest in local space
+        local_diff = local_rest.inverted() @ local_posed
+
+        return local_diff
+
     def modal(self, context, event):
         mytool = context.scene.my_tool
         global GetIcon
@@ -364,64 +404,76 @@ class SkeletalMeshModalTimer(bpy.types.Operator):
             self.cancel(context)
             cancel=False
             return {'CANCELLED'}
-        
+
         if event.type == 'TIMER':
             #bpy.data.objects["Cube"]
             global message1
-            if(mytool.my_enum2=="BC" and mytool.my_string!=""):                        
+            if(mytool.my_enum2=="BC" and mytool.my_string!=""):
                 count = 0
                 GetIcon="RADIOBUT_ON"
                 message1 = mytool.my_enum + "_"+mytool.my_string+"="
+                # Parse the bone names from the new property
+                selected_bones = [name.strip() for name in mytool.bone_names.split(',') if name.strip()]
+
                 for i in bpy.data.objects[mytool.my_string].pose.bones:
-                    #if(count < 3):
-                    #    count = count + 1
-                    #else:
-                    #    break
-                    #boneEdit = bpy.data.armatures['root'].bones[i.name].matrix_local.to_quaternion()
-                    obj = i.id_data
-                    matrix_final = obj.matrix_world @ i.matrix
-                    locationWS = i.location
-                    quaternionWS =i.rotation_quaternion# matrix_final.to_quaternion()
-                    #quaternionWS = i.rotation_quaternion * boneEdit
-                    #print(quaternionWS)
-                    #mixamo bone name conversion
-                    bone_name=i.name
-                    split_name=bone_name.split(":")[-1]
-                    message1+=split_name + ":(" + "{:.9f}".format(locationWS.x)+ "," + "{:.9f}".format(-locationWS.y) +  "," + "{:.9f}".format(-locationWS.z) +  "," + "{:.9f}".format(-quaternionWS.x) +  "," + "{:.9f}".format(quaternionWS.y)+  "," + "{:.9f}".format(-quaternionWS.z)+ "," + "{:.9f}".format(quaternionWS.w)+ ")" + "|"
+                    # mixamo bone name conversion
+                    bone_name = i.name.split(":")[-1]
+                    if not selected_bones or bone_name in selected_bones:  # Only send if no bones specified or if this bone is in the list
+                        local_matrix = SkeletalMeshModalTimer.get_pose_matrix_in_pose_space(i) # corrected line
+                        location = local_matrix.to_translation()
+                        rotation = local_matrix.to_quaternion()
+
+                        message1 += bone_name + ":(" + \
+                            "{:.9f}".format(location.x) + "," + \
+                            "{:.9f}".format(-location.y) + "," + \
+                            "{:.9f}".format(-location.z) + "," + \
+                            "{:.9f}".format(-rotation.x) + "," + \
+                            "{:.9f}".format(rotation.y) + "," + \
+                            "{:.9f}".format(-rotation.z) + "," + \
+                            "{:.9f}".format(rotation.w) + ")" + "|"
                 message1 = message1 + "|"
-            
+
             elif(mytool.my_enum2=="BC" and mytool.my_string==""):
                 MessageBox("No subject selected","Subjects")
                 GetIcon="RADIOBUT_OFF"
                 self.cancel(context)
                 return {'CANCELLED'}
-            
+
             elif(mytool.my_enum2=="AN" and mytool.my_string!=""):
                GetIcon="RADIOBUT_ON"
                for j in names_Arm:
                   message1+=mytool.my_enum + "_"+j+"="
+                  # Parse the bone names from the new property
+                  selected_bones = [name.strip() for name in mytool.bone_names.split(',') if name.strip()]
+
                   for i in bpy.data.objects[j].pose.bones:
-                      obj = i.id_data
-                      matrix_final = obj.matrix_world @ i.matrix
-                      locationWS = i.location
-                      quaternionWS =i.rotation_quaternion
-                      #mixamo bone name conversion
-                      bone_name=i.name
-                      split_name=bone_name.split(":")[-1]
-                      message1+=split_name + ":(" + "{:.9f}".format(locationWS.x)+ "," + "{:.9f}".format(-locationWS.y) +  "," + "{:.9f}".format(-locationWS.z) +  "," + "{:.9f}".format(-quaternionWS.x) +  "," + "{:.9f}".format(quaternionWS.y)+  "," + "{:.9f}".format(-quaternionWS.z)+ "," + "{:.9f}".format(quaternionWS.w)+ ")" + "|"
+                      bone_name = i.name.split(":")[-1]
+                      if not selected_bones or bone_name in selected_bones:  # Only send if no bones specified or if this bone is in the list
+                        local_matrix = SkeletalMeshModalTimer.get_pose_matrix_in_pose_space(i) # corrected line
+                        location = local_matrix.to_translation()
+                        rotation = local_matrix.to_quaternion()
+
+                        message1 += bone_name + ":(" + \
+                            "{:.9f}".format(location.x) + "," + \
+                            "{:.9f}".format(-location.y) + "," + \
+                            "{:.9f}".format(-location.z) + "," + \
+                            "{:.9f}".format(-rotation.x) + "," + \
+                            "{:.9f}".format(rotation.y) + "," + \
+                            "{:.9f}".format(-rotation.z) + "," + \
+                            "{:.9f}".format(rotation.w) + ")" + "|"
                   message1 = message1 + "|"
-                  
+
             elif(mytool.my_enum2=="AN" and mytool.my_string==""):
                 MessageBox("No skeletal mesh selected","Subjects")
                 GetIcon="RADIOBUT_OFF"
                 self.cancel(context)
                 return {'CANCELLED'}
-                      
+
             message=message1
             print(message)
             self.UDPSock.sendto(message.encode(), self.addr)
             message1=""
-            
+
         return {'PASS_THROUGH'}
 
     def execute(self, context):
@@ -452,7 +504,7 @@ classes = [AddSubjects,AddSubjects_Skeletal,RemoveSubjects,RemoveSubjects_Skelet
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-    
+
     bpy.types.Scene.my_tool2 = bpy.props.PointerProperty(type = StaticProperties)
     bpy.types.Scene.my_tool = bpy.props.PointerProperty(type = SkeletalProperties)
 
